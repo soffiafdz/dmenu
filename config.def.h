e144477ee7e49b1c691d6bb425bdb19555fbcfca
Soffiafdz <soffiafdz@gmail.com>
2021-04-13 15:31:50 -0400

    Patch: highPriority

--- config.def.h
+++ config.def.h
@@ -15,6 +15,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
