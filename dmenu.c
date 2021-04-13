d2a0d35a0c621742a1333464e70c0620b7357f6a
Soffiafdz <soffiafdz@gmail.com>
2021-04-13 19:12:25 -0400

    General and Specific Colors

--- dmenu.c
+++ dmenu.c
@@ -1061,43 +1061,53 @@ readxresources(void) {
 			fonts[0] = strdup(xval.addr);
 		else
 			fonts[0] = strdup(fonts[0]);
-		if (XrmGetResource(xdb, "dmenu.background", "*", &type, &xval))
+		if (XrmGetResource(xdb, "dmenu.background", "*", &type, &xval) ||
+			  XrmGetResource(xdb, "dmenu.color0", "*", &type, &xval))
 			colors[SchemeNorm][ColBg] = strdup(xval.addr);
 		else
 			colors[SchemeNorm][ColBg] = strdup(colors[SchemeNorm][ColBg]);
-		if (XrmGetResource(xdb, "dmenu.foreground", "*", &type, &xval))
+		if (XrmGetResource(xdb, "dmenu.foreground", "*", &type, &xval) ||
+			  XrmGetResource(xdb, "dmenu.color15", "*", &type, &xval))
 			colors[SchemeNorm][ColFg] = strdup(xval.addr);
 		else
 			colors[SchemeNorm][ColFg] = strdup(colors[SchemeNorm][ColFg]);
-		if (XrmGetResource(xdb, "dmenu.selbackground", "*", &type, &xval))
+		if (XrmGetResource(xdb, "dmenu.selbackground", "*", &type, &xval) ||
+			  XrmGetResource(xdb, "dmenu.color15", "*", &type, &xval))
 			colors[SchemeSel][ColBg] = strdup(xval.addr);
 		else
 			colors[SchemeSel][ColBg] = strdup(colors[SchemeSel][ColBg]);
-		if (XrmGetResource(xdb, "dmenu.selforeground", "*", &type, &xval))
+		if (XrmGetResource(xdb, "dmenu.selforeground", "*", &type, &xval) ||
+			  XrmGetResource(xdb, "dmenu.color0", "*", &type, &xval))
 			colors[SchemeSel][ColFg] = strdup(xval.addr);
 		else
 			colors[SchemeSel][ColFg] = strdup(colors[SchemeSel][ColFg]);
-		if (XrmGetResource(xdb, "dmenu.nhibackground", "*", &type, &xval))
+		if (XrmGetResource(xdb, "dmenu.nhibackground", "*", &type, &xval) ||
+			  XrmGetResource(xdb, "dmenu.color0", "*", &type, &xval))
 			colors[SchemeNormHighlight][ColBg] = strdup(xval.addr);
 		else
 			colors[SchemeNormHighlight][ColBg] = strdup(colors[SchemeNormHighlight][ColBg]);
-		if (XrmGetResource(xdb, "dmenu.nhiforeground", "*", &type, &xval))
+		if (XrmGetResource(xdb, "dmenu.nhiforeground", "*", &type, &xval) ||
+			  XrmGetResource(xdb, "dmenu.color3", "*", &type, &xval))
 			colors[SchemeNormHighlight][ColFg] = strdup(xval.addr);
 		else
 			colors[SchemeNormHighlight][ColFg] = strdup(colors[SchemeNormHighlight][ColFg]);
-		if (XrmGetResource(xdb, "dmenu.shibackground", "*", &type, &xval))
+		if (XrmGetResource(xdb, "dmenu.shibackground", "*", &type, &xval) ||
+			  XrmGetResource(xdb, "dmenu.color15", "*", &type, &xval))
 			colors[SchemeSelHighlight][ColBg] = strdup(xval.addr);
 		else
 			colors[SchemeSelHighlight][ColBg] = strdup(colors[SchemeSelHighlight][ColBg]);
-		if (XrmGetResource(xdb, "dmenu.shiforeground", "*", &type, &xval))
+		if (XrmGetResource(xdb, "dmenu.shiforeground", "*", &type, &xval) ||
+			  XrmGetResource(xdb, "dmenu.color3", "*", &type, &xval))
 			colors[SchemeSelHighlight][ColFg] = strdup(xval.addr);
 		else
 			colors[SchemeSelHighlight][ColFg] = strdup(colors[SchemeSelHighlight][ColFg]);
-		if (XrmGetResource(xdb, "dmenu.hipbackground", "*", &type, &xval))
+		if (XrmGetResource(xdb, "dmenu.hipbackground", "*", &type, &xval) ||
+			  XrmGetResource(xdb, "dmenu.color0", "*", &type, &xval))
 			colors[SchemeHp][ColBg] = strdup(xval.addr);
 		else
 			colors[SchemeHp][ColBg] = strdup(colors[SchemeHp][ColBg]);
-		if (XrmGetResource(xdb, "dmenu.hipforeground", "*", &type, &xval))
+		if (XrmGetResource(xdb, "dmenu.hipforeground", "*", &type, &xval) ||
+			  XrmGetResource(xdb, "dmenu.color1", "*", &type, &xval))
 			colors[SchemeHp][ColFg] = strdup(xval.addr);
 		else
 			colors[SchemeHp][ColFg] = strdup(colors[SchemeHp][ColFg]);
